a8ea2430b5cd0e7ef9e17220538ebb8db1bcabab
Aidan Barton <aidan.barton@rackspace.com>
2017-05-29 11:49:22 -0400

    Testing flask

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+__all__ = ("app")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html',
+    )
+
+
+def run_debug():
+    site_host = "0.0.0.0"
+    site_port = 3000
+    site_debug = True
+    app.run(
+        host=site_host, port=site_port,
+        debug=site_debug, use_debugger=site_debug)
+
+
+if "__main__" == __name__:
+    run_debug()
