350d4a0371a28e282ab79340540952cd83aa2f15
aidan <tonaid@vt.edu>
2017-08-14 18:49:02 -0400

    updated

--- app/app.py
+++ app/app.py
@@ -6,7 +6,10 @@ app.url_map.strict_slashes = False
 def get_nav(cur_page):
     nav_hrefs = {
                   'exper': '/exper',
-                  'about': '/about'
+                  'about': '/about',
+                  'exper_vt': '/exper/vt',
+                  'exper_hu': '/exper/hu',
+                  'exper_rs': '/exper/rs',
                 }
     nav_hrefs[cur_page] = '#'
     return nav_hrefs
@@ -18,31 +21,36 @@ def index():
 @app.route('/exper')
 def exper():
     return render_template('index.html',
-                           nav_hrefs = get_nav('exper')
+                           nav_hrefs = get_nav('exper'),
+                           exper_sub = None
                           )
 
 @app.route('/exper/vt')
 def exper_vt():
     return render_template('vt.html',
-                           nav_hrefs = get_nav('exper_vt')
+                           nav_hrefs = get_nav('exper_vt'),
+                           exper_sub = 'vt'
                           )
 
 @app.route('/exper/hu')
 def exper_hu():
     return render_template('hu.html',
-                           nav_hrefs = get_nav('exper_hu')
+                           nav_hrefs = get_nav('exper_hu'),
+                           exper_sub = 'hu'
                           )
 
 @app.route('/exper/rs')
 def exper_rs():
-    return render_template('vs.html',
-                           nav_hrefs = get_nav('exper_rs')
+    return render_template('rs.html',
+                           nav_hrefs = get_nav('exper_rs'),
+                           exper_sub = 'rs'
                           )
 
 @app.route('/about')
 def about():
     return render_template('about.html',
-                           nav_hrefs = get_nav('about')
+                           nav_hrefs = get_nav('about'),
+                           exper_sub = None
                           )
 
 
